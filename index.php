9d7f9ef50be3fdf10b64e1f4782870d19e8d87d7
Taufik Nurrohman <t.nurrohman77@gmail.com>
2022-01-08 02:25:44 +0700

    Update

--- index.php
+++ index.php
@@ -202,21 +202,16 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
             exit;
         }
         $keys = [];
-        foreach ($_POST['keys']['name'] as $k => $v) {
-            $rule = "";
-            if (isset($_POST['keys']['auto-increment'][$k])) {
-                $rule .= 'AUTOINCREMENT';
+        foreach ($_POST['keys']['key'] as $k => $v) {
+            $rules = "";
+            foreach ($_POST['keys']['rules'][$k] ?? [] as $kk => $vv) {
+                $rules .= ' ' . $kk;
             }
-            if (isset($_POST['keys']['primary-key'][$k])) {
-                $rule .= ' PRIMARY KEY';
-            }
-            if (isset($_POST['keys']['unique'][$k])) {
-                $rule .= ' UNIQUE';
-            }
-            if (isset($_POST['keys']['not-null'][$k])) {
-                $rule .= ' NOT NULL';
-            }
-            $keys[$v] = trim(($_POST['keys']['type'][$k] ?? 'TEXT') . ' ' . $rule);
+            $chops = explode(' ', strtr($_POST['keys']['type'][$k] ?? 'TEXT', [
+                ':default' => $_POST['keys']['value'] ?? 'NULL',
+                ':key' => $v
+            ]), 2);
+            $keys[$v] = trim($chops[0] . $rules . ' ' . ($chops[1] ?? ""));
         }
         if (Base::create($_POST['table'], $keys)) {
             $_SESSION['alert'] = 'Created table <code>' . $_POST['table'] . '</code>.';
@@ -285,38 +280,175 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
     <style>
 
     * {
+      background: none;
       box-sizing: border-box;
+      color: inherit;
+      font: inherit;
+      margin: 0;
+      padding: 0;
+    }
+
+    :focus {
+      outline: 0;
     }
 
     :root {
       background: #fff;
       color: #000;
-      font-family: 'times new roman', serif;
+      font: normal normal 18px/1.4 sans-serif;
+      padding: 1em;
+    }
+
+    a {
+      color: #00f;
+      text-decoration: none;
+    }
+
+    a:focus,
+    a:hover {
+      text-decoration: underline;
+    }
+
+    b, th {
+      font-weight: bold;
     }
 
     button {
+      background: #def;
+      border: 2px solid #000;
       cursor: pointer;
+      padding: .25em .5em;
+    }
+
+    button:focus,
+    select:focus {
+      border-color: #00f;
+      outline-offset: -4px;
+      outline: 1px solid #00f;
+    }
+
+    i {
+      font-style: italic;
+    }
+
+    input[type='number'],
+    input[type='text'],
+    select,
+    textarea {
+      background: #fff;
+      border: 2px solid #000;
+      padding: .25em .5em;
+    }
+
+    input[type='checkbox'],
+    input[type='radio'] {
+      appearance: none;
+      border: 2px solid;
+      height: 1em;
+      margin-top: .125em;
+      min-height: 1em;
+      min-width: 1em;
+      width: 1em;
+    }
+
+    input[type='checkbox']:focus,
+    input[type='radio']:focus {
+      border-color: #00f;
+      outline: 0;
+    }
+
+    input[type='checkbox']:checked,
+    input[type='radio']:checked {
+      background: #00f;
+      box-shadow: inset 0 0 0 3px #fff;
+    }
+
+    input[type='radio'] {
+      border-radius: 100%;
+    }
+
+    hr,
+    p,
+    table {
+      margin: 1em 0;
+    }
+
+    hr {
+      border: 0;
+      border-top: 1px dashed #000;
     }
 
     select {
       cursor: pointer;
     }
 
-    th, td {
+    small {
+      font-size: smaller;
+    }
+
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+    th {
+      border: 1px solid;
+      padding: .25em;
       text-align: left;
       vertical-align: top;
     }
 
-    body > p:first-child {
+    tfoot td,
+    tfoot th {
+      background: #eee;
+    }
+
+    [role='alert'] {
       background: #ff0;
       padding: .35em .5em;
+    }
+
+    #table-rows-container li,
+    #table-rows-container p,
+    #table-rows-container ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+
+    #table-rows-container ul ul {
+      margin-left: 1.5em;
+    }
+
+    #table-rows-container ul label {
+      align-items: start;
+      cursor: pointer;
+      display: flex;
+      gap: .5em;
+      user-select: none;
+    }
+
+    :disabled {
+      cursor: not-allowed;
+      opacity: .5;
+    }
+
+    :focus:invalid {
+      border-color: #f00;
+      color: #f00;
+      outline-color: #f00;
+    }
+
+    [hidden] {
+      display: none !important;
     }
 
     </style>
   </head>
   <body>
     <?php if (isset($_SESSION['alert'])): ?>
-      <p>
+      <p role="alert">
         <?= $_SESSION['alert']; ?>
       </p>
     <?php endif; ?>
@@ -327,7 +459,7 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
         </button>
       <?php else: ?>
         <button name="task" type="submit" value="create">
-          Create
+          New Table
         </button>
       <?php endif; ?>
     </form>
@@ -340,14 +472,14 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
               <b>Table Name</b>
             </label>
             <p>
-              <input autofocus name="table" pattern="^[A-Z_][a-zA-Z\d_]*(?:_[A-Z\d][a-zA-Z\d]*)*$" placeholder="FooBarBaz" required type="text">
+              <input autofocus id="table-name" name="table" pattern="^[A-Z_][a-zA-Z\d_]*(?:_[A-Z\d][a-zA-Z\d]*)*$" placeholder="FooBarBaz" required type="text">
             </p>
           </p>
           <p>
             <label>
               <b>Table Columns</b>
             </label>
-            <table border="1">
+            <table hidden>
               <thead>
                 <th>
                   Name
@@ -356,64 +488,94 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
                   Type
                 </th>
                 <th>
-                  Status
+                  Actions
                 </th>
-                <th></th>
               </thead>
-              <tbody>
-                <tr>
-                  <td colspan="4">
-                    <button onclick="addColumn.call(this);" title="Add Column" type="button">
-                      Add
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
+              <tbody id="table-rows-container"></tbody>
             </table>
           </p>
           <p>
-            <button type="submit">
-              Create
+            <button class="add-table-column" disabled type="button">
+              Add Column
+            </button>
+            <button class="create-table" disabled type="submit">
+              Create Table
             </button>
           </p>
-          <template id="table-column">
+          <template id="table-column-template">
             <tr>
               <td>
-                <input name="keys[name][]" pattern="^[a-zA-Z_][a-zA-Z\d_]*(?:_[a-zA-Z\d]*)*$" placeholder="fooBarBaz" required type="text">
+                <input name="keys[key][]" pattern="^[a-zA-Z_][a-zA-Z\d_]*(?:_[a-zA-Z\d]*)*$" placeholder="fooBarBaz" required type="text">
               </td>
               <td>
-                <select name="keys[type][]" onchange="changeStatus.call(this);">
-                  <option selected value="TEXT">Text</option>
-                  <option value="BLOB">Blob</option>
-                  <option value="INTEGER">Number</option>
-                  <option value="INTEGER">Toggle</option>
-                  <option value="NULL">Null</option>
-                  <option value="REAL">Decimal</option>
-                </select>
+                <ul>
+                  <?php
+
+                  $types = [
+                      'BLOB' => 'File',
+                      'INTEGER DEFAULT 0 CHECK (:key IN (0, 1))' => 'Boolean',
+                      'INTEGER' => 'Number',
+                      'NULL' => 'Null',
+                      'REAL' => 'Decimal',
+                      'TEXT' => 'Any'
+                  ];
+
+                  asort($types);
+
+                  ?>
+                  <?php foreach ($types as $k => $v): ?>
+                    <li>
+                      <p>
+                        <label>
+                          <input class="the-table-column"<?= 'TEXT' === $k ? ' checked' : ""; ?> name="keys[type][]" type="radio" value="<?= htmlspecialchars($k); ?>">
+                          <span>
+                            <?= $v; ?>
+                          </span>
+                        </label>
+                      </p>
+                      <p<?= 'TEXT' === $k ? "" : ' hidden'; ?>>
+                        <b>Rules</b>
+                      </p>
+                      <ul<?= 'TEXT' === $k ? "" : ' hidden'; ?>>
+                        <li>
+                          <label>
+                            <input name="keys[rules][][AUTOINCREMENT]" type="checkbox">
+                            <span>
+                              Automatically increment the value on this field if it was not set explicitly by the user. The automatic value is made based on the last number that has been inserted in the previous action.
+                            </span>
+                          </label>
+                        </li>
+                        <li>
+                          <label>
+                            <input name="keys[rules][][NOT NULL]" type="checkbox">
+                            <span>
+                              Force user to provide specific data on this field, or else the insertion process will be rejected.
+                            </span>
+                          </label>
+                        </li>
+                        <li>
+                          <label>
+                            <input name="keys[rules][][PRIMARY KEY]" type="checkbox">
+                            <span>
+                              Make this field as the primary key.
+                            </span>
+                          </label>
+                        </li>
+                        <li>
+                          <label>
+                            <input name="keys[rules][][UNIQUE]" type="checkbox">
+                            <span>
+                              Make sure this field rejects the given value if it already exists in other records.
+                            </span>
+                          </label>
+                        </li>
+                      </ul>
+                    </li>
+                  <?php endforeach; ?>
+                </ul>
               </td>
               <td>
-                <label>
-                  <input disabled name="keys[auto-increment][]" type="checkbox" value="1">
-                  Auto Increment
-                </label>
-                <br>
-                <label>
-                  <input name="keys[not-null][]" type="checkbox" value="1">
-                  Not Null
-                </label>
-                <br>
-                <label>
-                  <input name="keys[primary-key][]" type="checkbox" value="1">
-                  Primary Key
-                </label>
-                <br>
-                <label>
-                  <input name="keys[unique][]" type="checkbox" value="1">
-                  Unique
-                </label>
-              </td>
-              <td>
-                <button onclick="removeColumn.call(this);" title="Remove This Column" type="button">
+                <button class="remove-table-column" type="button">
                   Remove
                 </button>
               </td>
@@ -421,35 +583,109 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
           </template>
           <script>
 
-          const column = document.querySelector('#table-column');
+          const tableColumnAdd = document.querySelector('.add-table-column');
+          const tableColumnTemplate = document.querySelector('#table-column-template');
+          const tableCreate = document.querySelector('.create-table');
+          const tableName = document.querySelector('#table-name');
+          const tableRowsContainer = document.querySelector('#table-rows-container');
+
+          let index = 0;
 
           function addColumn() {
-              let parent = this.parentNode.parentNode,
-                  clone = column.content.cloneNode(true);
-              parent.parentNode.insertBefore(clone, parent);
-              parent.previousElementSibling.querySelector('input').focus();
+              tableRowsContainer.appendChild(tableColumnTemplate.content.cloneNode(true));
+              setTimeout(() => {
+                  document.querySelectorAll('[name]').forEach(input => {
+                      input.name = input.name.replace(/\[\]/g, '[' + index + ']');
+                  });
+                  let focus = [...tableRowsContainer.querySelectorAll('input[type="text"]')].pop();
+                  focus && focus.focus();
+                  document.querySelectorAll('.remove-table-column:not(.has-event)').forEach(remove => {
+                      remove.addEventListener('click', removeColumn, false);
+                      remove.classList.add('has-event');
+                  });
+                  document.querySelectorAll('.the-table-column:not(.has-event)').forEach(the => {
+                      the.addEventListener('change', changeType, false);
+                      the.classList.add('has-event');
+                  });
+                  ++index;
+              }, 1);
+              checkTable();
           }
 
-          function changeStatus() {
-              let value = this.value,
-                  parent = this.parentNode,
-                  next = parent.nextElementSibling,
-                  autoIncrement = next.querySelector('[name="keys[auto-increment][]"]');
-              if ('INTEGER' === value) {
-                  autoIncrement.disabled = false;
-              } else {
-                  autoIncrement.disabled = true;
-              }
+          function changeType() {
+              document.querySelectorAll('.the-table-column').forEach(the => {
+                  the.parentNode.parentNode.nextElementSibling.hidden = true;
+                  the.parentNode.parentNode.nextElementSibling.nextElementSibling.hidden = true;
+              });
+              this.parentNode.parentNode.nextElementSibling.hidden = false;
+              this.parentNode.parentNode.nextElementSibling.nextElementSibling.hidden = false;
+          }
+
+          function checkTable() {
+              tableRowsContainer.closest('table').hidden = 0 === tableRowsContainer.children.length;
+          }
+
+          function checkTableName() {
+              let valid = tableName.validity && tableName.validity.valid;
+              tableColumnAdd.disabled = !valid;
+              tableCreate.disabled = !valid;
           }
 
           function removeColumn() {
               this.closest('tr').remove();
+              checkTable();
+              --index;
           }
+
+          tableColumnAdd.addEventListener('click', addColumn, false);
+
+          tableName.addEventListener('input', checkTableName, false);
+          tableName.addEventListener('keyup', checkTableName, false);
 
           </script>
         <?php elseif ('update' === $task): ?>
           <?php if ($table = Base::table($_GET['table'])): ?>
-            <table border="1" style="width: 100%;">
+            <table>
+              <thead>
+                <tr>
+                  <?php foreach ($table as $k => $v): ?>
+                    <th>
+                      <?= $k; ?>
+                    </th>
+                  <?php endforeach; ?>
+                  <th></th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <?php foreach ($table as $k => $v): ?>
+                    <?php if ('ID' === $k): ?>
+                      <td></td>
+                    <?php else: ?>
+                      <td>
+                        <?php if ('BLOB' === $v): ?>
+                          <input name="values[<?= $k; ?>]" style="display: block; width: 100%;" type="file">
+                        <?php elseif ('INTEGER' === $v): ?>
+                          <input name="values[<?= $k; ?>]" style="display: block; width: 100%;" type="number">
+                        <?php elseif ('NULL' === $v): ?>
+                          <em>NULL</em>
+                        <?php elseif ('REAL' === $v): ?>
+                          <input name="values[<?= $k; ?>]" style="display: block; width: 100%;" type="number">
+                        <?php else: ?>
+                          <textarea name="values[<?= $k; ?>]" style="display: block; width: 100%;"></textarea>
+                        <?php endif; ?>
+                      </td>
+                    <?php endif; ?>
+                  <?php endforeach; ?>
+                  <td>
+                    <button type="submit">
+                      Insert
+                    </button>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+            <table>
               <thead>
                 <tr>
                   <?php foreach ($table as $k => $v): ?>
@@ -480,37 +716,8 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
                   <?php endforeach; ?>
                 </tbody>
               <?php endif; ?>
-              <tfoot>
-                <tr>
-                  <?php foreach ($table as $k => $v): ?>
-                    <?php if ('ID' === $k): ?>
-                      <td></td>
-                    <?php else: ?>
-                      <td>
-                        <?php if ('BLOB' === $v): ?>
-                          <input name="values[<?= $k; ?>]" style="display: block; width: 100%;" type="file">
-                        <?php elseif ('INTEGER' === $v): ?>
-                          <input name="values[<?= $k; ?>]" style="display: block; width: 100%;" type="number">
-                        <?php elseif ('NULL' === $v): ?>
-                          <em>NULL</em>
-                        <?php elseif ('REAL' === $v): ?>
-                          <input name="values[<?= $k; ?>]" style="display: block; width: 100%;" type="number">
-                        <?php else: ?>
-                          <textarea name="values[<?= $k; ?>]" style="display: block; width: 100%;"></textarea>
-                        <?php endif; ?>
-                      </td>
-                    <?php endif; ?>
-                  <?php endforeach; ?>
-                  <td></td>
-                </tr>
-              </tfoot>
             </table>
             <input name="table" type="hidden" value="<?= $_GET['table']; ?>">
-            <p>
-              <button type="submit">
-                Insert
-              </button>
-            </p>
           <?php else: ?>
             <p>
               Table <code><?= $_GET['table']; ?></code> does not exist.
@@ -519,7 +726,7 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
         <?php endif; ?>
       <?php else: ?>
         <?php if ($tables = (array) Base::tables()): ?>
-          <table border="1">
+          <table>
             <thead>
               <tr>
                 <th>
@@ -532,9 +739,9 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
               <?php foreach ($tables as $k => $v): ?>
                 <tr>
                   <td>
-                    <a href="<?= $p; ?>?table=<?= $k; ?>&amp;task=update">
-                      <?= $k; ?>
-                    </a> (<?= $v . ' Row' . (1 === $v ? "" : 's'); ?>)
+                    <a href="<?= $p; ?>?table=<?= $k; ?>&amp;task=update"><?= $k; ?></a>
+                    <br>
+                    <small><?= $v . ' Row' . (1 === $v ? "" : 's'); ?></small>
                   </td>
                   <td>
                     <button name="drop" onclick="return confirm('Are you sure you want to delete this table with its rows?')" type="submit" value="<?= $k; ?>">
